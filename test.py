5b811eb05ce4b05fda00fd4cc13788912093044e
kimzapp <giaphk2012004@gmail.com>
2026-05-26 11:13:13 +0700

    chores

--- test.py
+++
@@ -1,9 +0,0 @@
-from optimum.exporters.onnx import main_export
-
-main_export(
-    model_name_or_path="vinai/PhoGPT-4B",
-    output="phogpt_onnx",
-    task="text-generation",
-    opset=14,
-    use_cache=True
-)
